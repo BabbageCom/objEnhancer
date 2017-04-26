ab015a29477a02809226d650cb45dcb92ee8fef6
Leonard de Ruijter <leonard@babbage.com>
2017-04-26 17:19:16 +0200

    Added dialogs module

---
+++ addon/globalPlugins/objEnhancer/dialogs.py
@@ -0,0 +1,11 @@
+# Object Enhancer
+
+#Copyright (C) 2017 Babbage B.V.
+
+#This file is covered by the GNU General Public License.
+#See the file COPYING for more details.
+
+import wx
+import gui
+import configobj
+import controlTypes

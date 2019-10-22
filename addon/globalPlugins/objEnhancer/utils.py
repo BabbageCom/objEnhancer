0913290df9b8eefc47998fa3a12a2e125882a95b
Leonard de Ruijter <leonard@babbage.com>
2019-10-22 12:01:03 +0200

    Ensure obj enhancer digest is saved as unicode

--- addon/globalPlugins/objEnhancer/utils.py
+++ addon/globalPlugins/objEnhancer/utils.py
@@ -7,7 +7,7 @@ def locationBitmapHash(location):
 	sb = screenBitmap.ScreenBitmap(location.width, location.height)
 	pixels = sb.captureImage(*location)
 	digest = hashlib.sha1(bytes(pixels)).digest()
-	return base64.b64encode(digest)
+	return base64.b64encode(digest).decode("ascii")
 
 
 def bitmapHash(obj, padding=10):
